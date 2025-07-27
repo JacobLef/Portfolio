96dbe356c5395dfbf8ac4e17759a88f08b6297a7
JacobLef <jacoblefk@gmail.com>
2025-07-27 11:49:18 -0400

    feat(src/components/sections): Fixed issue with the Projects section being linked to the incorrect identifier.

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -3,7 +3,7 @@ import { FaGithub } from 'react-icons/fa';
 export const Projects = () =>
 {
   return (
-    <section id="#projects" className="min-h-screen flex items-center justify-center py-20">
+    <section id="projects" className="min-h-screen flex items-center justify-center py-20">
       <div className="max-w-5xl mx-auto px-4">
         <h2
           className="text-3xl font-bold mb-8 bg-gradient-to-r from-red-500 to-orange-400 bg-clip-text text-transparent text-center"
