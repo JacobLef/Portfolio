696973e62afef1031c4c69f424f04a0317652c54
JacobLef <jacoblefk@gmail.com>
2025-07-27 11:47:05 -0400

    Changed the Projects file to be a jsx file and not a tsx file. Fixed importing issues with the index.css file not recognizing this file as exisitng in the project anymore.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { NavBar } from './components/Navbar'
 import { MobileMenu } from './components/MobileMenu'
 import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
-import { Projects } from "./components/sections/Projects"
+import { Projects } from "./components/sections/Projects";
 import "./index.css"
 
 function App() {
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { FaGithub } from 'react-icons/fa';
 
 export const Projects = () =>
